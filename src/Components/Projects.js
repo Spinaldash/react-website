5c004a23a94358c95064f1acdab7d635e3aa2c01
MJ Perez <blueavenger@gmail.com>
2017-05-31 19:35:14 -0700

    experimenting with semantic-ui-react

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Image, Header } from 'semantic-ui-react'
 
 const brewSub = `The Planeswalker's Compendium`
+const gameSub = `Trade your games`
+const brew2Sub = `The Planeswalker's Compendium (Rebuilt with React)`
 
 const Projects = () => (
   <div>
@@ -15,11 +17,20 @@ const Projects = () => (
       </Header.Content>
     </Header>
     <Header as='h2'>
+      <Image src='https://c1.staticflickr.com/5/4268/34162321114_fc24b1b3ba.jpg' />
+      <Header.Content>
+        Gameswap
+        <Header.Subheader>
+          {gameSub}
+        </Header.Subheader>
+      </Header.Content>
+    </Header>
+    <Header as='h2'>
       <Image src='https://c1.staticflickr.com/5/4251/34965091716_29fb270b1c.jpg' />
       <Header.Content>
-        Brew
+        Brew 2
         <Header.Subheader>
-          {brewSub}
+          {brew2Sub}
         </Header.Subheader>
       </Header.Content>
     </Header>
